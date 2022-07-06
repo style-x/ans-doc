05a1fcbbcd7f698bd6f95079827afac11203065a
xan <47989552+style-x@users.noreply.github.com>
2022-07-06 20:27:50 +0200

    Add Searchbar
    Suchleiste zugefügt und erste Funktion, aber Filter funktioniert noch nicht.

--- app.js
+++ app.js
@@ -1,40 +1,61 @@
 // Anfang
 
-var nav = document.querySelector('nav');
-var main = document.querySelector('main');
-
-// https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json
-var requestURL = 'https://style-x.github.io/ans-doc/db.json';
-
-console.log(requestURL);
-
-var request = new XMLHttpRequest();
+let nav = document.querySelector('nav');
+let main = document.querySelector('main');
+let requestURL = 'https://style-x.github.io/ans-doc/db.json';
+let request = new XMLHttpRequest();
+var result;
+var data;
 
 request.open('GET', requestURL);
-
 request.responseType = 'json';
 request.send();
 
-function buildTable(jsonObj) {
-  var table = document.getElementById('myTable');
-  let data = jsonObj.article;
+let searchbar = document.getElementById('search-input');
+searchbar.addEventListener('keyup', function() {
+  let value = this.value;
+  //console.log(value);
+  search(value, data);
+  buildTable(data);
+})
+
+function buildTable(result) {
+  var table = document.getElementById('table');
+  data = result.article;
   table.innerHTML = '';
 
   for (i = 0; i < data.length; i++) {
-    while (i < 10) {
-      var row = `<tr>
-                    <td>${data[i].name}</td>
-                    <td>${data[i].artnr}</td>
-                    <td>${data[i].test}</td>
-                </tr>`
-      table.innerHTML += row;
-    };
-  }
+    var row = `<tr>
+                  <td>${data[i].name}</td>
+                  <td>${data[i].artnr}</td>
+                  <td>${data[i].clients}</td>
+              </tr>`
+    table.innerHTML += row;
+  };
 }
 
 request.onload = function() {
-  var myTable = request.response;
-  buildTable(myTable);
+  result = request.response;
+  buildTable(result);
+}
+
+function search(value, data) {
+  let filteredData = [];
+
+  console.log(data);
+
+  for (i = 0; i < data.length; i++) {
+    value = value.toLowerCase();
+    let name = data[i].name.toLowerCase();
+
+    console.log(value);
+
+    if (name.includes(value)){
+      filteredData.push(data[i])
+    }
+
+  };
+  return filteredData;
 }
 
 // Ende
